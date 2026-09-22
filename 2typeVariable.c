7597adeec5189c4d0bb76269a4d104925ee47c0d
Ian Souza Molina <20251141130056@ifrn.local>
2026-09-22 14:04:28 -0300

    Anotações

--- 2typeVariable.c
+++ 2typeVariable.c
@@ -3,11 +3,13 @@
 
 int main()
 {
-    int rpm;
-    float temperature;
-   printf("digite valor para RPM e a temperatura:");
-   scanf("%d %f", &rpm, &temperature);
-   printf("| rpm: %d | Temp: %.2fC |\n",rpm, temperature);
- 
+    int Sensor_value;
+    char sensor_state;// 'L' or 'H'
+   printf("Sensor Value:");
+   scanf("%d", &Sensor_value);
+   printf("Sensor state: high or Low");
+   scanf(" %c", &sensor_state);
+   printf("| Sensor Value: %d | State:(%c)\n", Sensor_value, sensor_state);
+
     return 0;
-}
+   }
--- Anotações.c
+++ Anotações.c
@@ -1,5 +1,14 @@
 /*
-/d = inteiro; %.xf = float onde x é o número de casas decimais
+%d= inteiro;
+%.xf = float onde x é o número de casas 
+%.c = char;
+
+|--------------------------OBS--------------------------------|
+|quando for ler um char, é necessário colocar um espaço       |
+| %c no scanf() para evitar que o buffer de entrada seja lido |
+|incorretamente.                                              |
+|-------------------------------------------------------------|
+
 printf() = saída (terminal)
 scanf() = entrada (teclado)
 fgets() = leitura de linha (texto com espaço)
